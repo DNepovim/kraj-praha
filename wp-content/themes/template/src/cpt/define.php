870efbbc70621cc34ddcdda89363cb3ae59b48f0
DNepovim <blaha.dominik.2@gmail.com>
2016-08-12 13:55:29 +0200

    Add post excerpt field to page post type

--- wp-content/themes/template/src/cpt/define.php
+++ wp-content/themes/template/src/cpt/define.php
@@ -4,6 +4,7 @@ add_action('init', 'create_post_type');
 
 function create_post_type()
 {
+    add_post_type_support('page', 'excerpt');
     register_post_type('event',
         array(
             'labels' => array(
