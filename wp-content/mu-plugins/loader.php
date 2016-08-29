127d6a754030fc37bdcd8ca8eb87db6a12036872
DNepovim <blaha.dominik.2@gmail.com>
2016-08-29 21:58:22 +0200

    Add mu-plugins loader to git.

---
+++ wp-content/mu-plugins/loader.php
@@ -0,0 +1,8 @@
+<?php
+
+require WPMU_PLUGIN_DIR.'/wp-tracy/index.php';
+require WPMU_PLUGIN_DIR.'/meta-box/meta-box.php';
+require WPMU_PLUGIN_DIR.'/html-editor-syntax-highlighter/html-editor-syntax-highlighter.php';
+require WPMU_PLUGIN_DIR.'/feedwordpress/feedwordpress.php';
+require WPMU_PLUGIN_DIR.'/fb-last-posts/fb-last-posts.php';
+//require WPMU_PLUGIN_DIR.'/fb-posts-to-cpt/fb-posts-to-cpt.php';
