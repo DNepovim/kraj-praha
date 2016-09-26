1e961175d5b74b7ae6430ae566faf1bdb592482d
DNepovim <blaha.dominik.2@gmail.com>
2016-09-26 21:15:37 +0200

    Remove unused CPT

--- wp-content/themes/template/src/cpt/define.php
+++ wp-content/themes/template/src/cpt/define.php
@@ -4,31 +4,22 @@ add_action('init', 'create_post_type');
 
 function create_post_type()
 {
-    add_post_type_support('page', 'excerpt');
-    register_post_type('event',
-        array(
-            'labels' => array(
-                'name' => __('Událost'),
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'supports' => array(
-	            'title',
-	            'author',
-	            'excerpt',
-	            'editor',
-	            'thumbnail',
-            )
-        )
-    );
-    register_post_type('krizovatka',
-        array(
-            'labels' => array(
-                'name' => __('Křižovatka'),
-            ),
-            'public' => true,
-            'has_archive' => true,
-        )
-    );
+	add_post_type_support('page', 'excerpt');
+	register_post_type('event',
+		array(
+			'labels' => array(
+				'name' => __('Událost'),
+			),
+			'public' => true,
+			'has_archive' => true,
+			'supports' => array(
+				'title',
+				'author',
+				'excerpt',
+				'editor',
+				'thumbnail',
+			)
+		)
+	);
 }
 
