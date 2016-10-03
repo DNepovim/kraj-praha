4f9135266926190b1732d41ae802cf7499db7ee5
DNepovim <jsem@dominikblaha.cz>
2016-10-03 16:58:36 +0200

    Add smaller thumbnails

--- wp-content/themes/template/functions.php
+++ wp-content/themes/template/functions.php
@@ -10,5 +10,5 @@ register_nav_menu('main_menu', 'Hlavní menu');
 // Register thumbnail
 add_theme_support( 'post-thumbnails' );
 set_post_thumbnail_size( 1200, 9999 );
-add_image_size('feed_thumb', 300, 300);
+add_image_size('post_thumb', 150, 150);
 
