5d0b91bbd2eebf33842974fcfc84c783525c4a77
DNepovim <jsem@dominikblaha.cz>
2016-12-23 14:38:29 +0100

    Show image preview in form

--- wp-content/themes/template/src/scripts/main.js
+++ wp-content/themes/template/src/scripts/main.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
 
     $('#primaryPostForm').validate()
 
+    // Image preview in upload input
+    $('.form__input--upload').on('change', function () {
+        $('#img_url')[0].src = (window.URL ? URL : webkitURL).createObjectURL(this.files[0]);
+    })
+
 });
