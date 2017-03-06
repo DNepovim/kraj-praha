452a1d084efec1abb908ebe8650d39b85e2834b6
DNepovim <jsem@dominikblaha.cz>
2017-03-06 19:28:46 +0100

    Revert "Dont disable html editor"
    This reverts commit 1bb2c6fded0660063d908faddb4ea6d3ad39f94e.

--- wp-content/themes/template/functions.php
+++ wp-content/themes/template/functions.php
@@ -12,6 +12,13 @@ add_theme_support( 'post-thumbnails' );
 set_post_thumbnail_size( 1200, 9999 );
 add_image_size('post_thumb', 150, 150);
 
+// Disable html editor
+function editor_settings($settings) {
+	$settings['quicktags'] = false;
+	return $settings;
+}
+add_filter('wp_editor_settings', 'editor_settings');
+
 // Redirect blank search to frontpage
 function change_blank_search( $query_variables ) {
 	if(isset( $_GET['s'] ) && (strlen(preg_replace('/\s+/u','', $_GET['s'])) == 0)) {
