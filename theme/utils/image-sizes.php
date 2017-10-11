323f5ca73ff63e7614feef30722e883f3fddf2de
DNepovim <jsem@dominikblaha.cz>
2017-10-11 23:17:42 +0200

    Adjust singular content

--- theme/utils/image-sizes.php
+++ theme/utils/image-sizes.php
@@ -4,5 +4,6 @@ add_action('after_setup_theme', 'add_image_sizes');
 function add_image_sizes() {
 	add_image_size('post-thumbnails', 1200, 9999);
 	add_image_size('post_thumb', 150, 150, true);
+	add_image_size('post_singular', 350, 350);
 	add_image_size('og', 1200, 630);
 }
