9c262154c5d1ce62789aa58ae653078d0c37481d
Dominik Bláha <dominik.blaha@firma.seznam.cz>
2019-01-03 21:23:39 +0100

    Adjust mobile version

--- theme/utils/image-sizes.php
+++ theme/utils/image-sizes.php
@@ -5,5 +5,6 @@ function add_image_sizes() {
 	add_image_size('post-thumbnails', 1200, 9999);
 	add_image_size('post_thumb', 150, 150, true);
 	add_image_size('post_singular', 350, 350);
+	add_image_size('post_feed', 450, 450);
 	add_image_size('og', 1200, 630);
 }
