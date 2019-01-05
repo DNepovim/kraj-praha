e7f1ca26e857dd655c23ed81c30aac4066f8253e
Dominik Bláha <dominik.blaha@firma.seznam.cz>
2019-01-05 14:11:22 +0100

    Refactor related posts in singular

--- theme/page.php
+++ theme/page.php
@@ -1,4 +1,23 @@
 <?php
 
-// latte file has same name as this file
-view('singular');
+$args = [];
+$taxonomy = get_post_meta(get_the_ID(), 'praha_taxonomy', true);
+
+if ($taxonomy) {
+	$catName = strtolower(get_cat_name($taxonomy));
+	$args['relatedTitle'] = 'Články k tématu ' . $catName . ':';
+
+	$args['relatedQuery'] = new WP_Query([
+		'cat' => $taxonomy,
+		'posts_per_page' => 3,
+	]);
+
+	if ($args['relatedQuery']->max_num_pages > 1) {
+		$args['relatedButton'] = [
+			'label' => 'Všechny články k&nbsp;tématu ' . $catName,
+			'link' => get_category_link($taxonomy),
+		];
+	}
+}
+
+view('singular', $args);
--- theme/single.php
+++ theme/single.php
@@ -1,4 +1,22 @@
 <?php
 
-// latte file has same name as this file
-view('singular');
+$args = [];
+$postID = get_the_ID();
+$taxonomies = wp_get_post_categories($postID);
+
+if ($taxonomies) {
+	$args['relatedTitle'] = 'Podobné články:';
+
+	$args['relatedQuery'] = new WP_Query([
+		'category__in' => $taxonomies,
+		'post__not_in' => [$postID],
+		'posts_per_page' => 3,
+	]);
+
+	$args['relatedButton'] = [
+		'label' => 'Všechny články',
+		'link' => '/',
+	];
+}
+
+view('singular', $args);
