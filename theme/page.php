bb006a9f7329d5fcf8ae26735efc78fcc275985a
Dominik Bláha <dominik.blaha@firma.seznam.cz>
2019-01-10 01:31:18 +0100

    Fix related posts show condition

--- theme/page.php
+++ theme/page.php
@@ -4,19 +4,25 @@ $args = [];
 $taxonomy = get_post_meta(get_the_ID(), 'praha_taxonomy', true);
 
 if ($taxonomy) {
-	$catName = strtolower(get_cat_name($taxonomy));
-	$args['relatedTitle'] = 'Články k tématu ' . $catName . ':';
 
-	$args['relatedQuery'] = new WP_Query([
+	$relatedQuery = new WP_Query([
 		'cat' => $taxonomy,
 		'posts_per_page' => 3,
 	]);
 
-	if ($args['relatedQuery']->max_num_pages > 1) {
-		$args['relatedButton'] = [
-			'label' => 'Všechny články k&nbsp;tématu ' . $catName,
-			'link' => get_category_link($taxonomy),
-		];
+	if ($relatedQuery->post_count) {
+		$args['relatedQuery'] = $relatedQuery;
+
+		$catName = strtolower(get_cat_name($taxonomy));
+
+		$args['relatedTitle'] = 'Články k tématu ' . $catName . ':';
+
+		if ($args['relatedQuery']->max_num_pages > 1) {
+			$args['relatedButton'] = [
+				'label' => 'Všechny články k&nbsp;tématu ' . $catName,
+				'link' => get_category_link($taxonomy),
+			];
+		}
 	}
 }
 
