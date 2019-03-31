89ac2d5f549b937ec00b0dc6527a6a32f83e4bf7
Dominik Bláha <dominik.blaha@firma.seznam.cz>
2019-03-31 16:40:51 +0200

    Crop image thumbnails

--- theme/utils/image-sizes.php
+++ theme/utils/image-sizes.php
@@ -2,15 +2,15 @@
 
 add_action('after_setup_theme', 'add_image_sizes');
 function add_image_sizes() {
-	add_image_size('100', 100);
-	add_image_size('150', 150);
-	add_image_size('200', 200);
-	add_image_size('300', 300);
-	add_image_size('500', 500);
-	add_image_size('700', 700);
-	add_image_size('900', 900);
-	add_image_size('1200', 1200);
-	add_image_size('1600', 1600);
+	add_image_size('100', 100, 100, ['center', 'top']);
+	add_image_size('150', 150, 150, ['center', 'top']);
+	add_image_size('200', 200, 200, ['center', 'top']);
+	add_image_size('300', 300, 300, ['center', 'top']);
+	add_image_size('500', 500, 500, ['center', 'top']);
+	add_image_size('700', 700, 700, ['center', 'top']);
+	add_image_size('900', 900, 900, ['center', 'top']);
+	add_image_size('1200', 1200, 1200, ['center', 'top']);
+	add_image_size('1600', 1600, 1600, ['center', 'top']);
 
 	add_image_size('post_detail', 330, 330);
 	add_image_size('post_feed', 450, 450);
