c322d4a635e9be0f991d02c494ee0f13a8d53e27
Dominik Bláha <jsem@dominikblaha.cz>
2021-11-11 10:17:24 +0100

    Fix and refactor links and buttons

--- theme/shortcodes/button.php
+++ theme/shortcodes/button.php
@@ -1,6 +1,14 @@
 <?php
 function shortcode_button($atts) {
-	return renderLatteToString(__DIR__ . '/../views/components/button.latte', $atts);
+	return renderLatteToString(__DIR__ . '/../views/components/button.latte', [
+		"label" => !empty($atts["label"]) ? $atts["label"] : "",
+		"style" => !empty($atts["style"]) ? $atts["style"] : "inv",
+		"attrs" => [
+			"href" => !empty($atts["link"]) ? $atts["link"] : "",
+			"target" => !empty($atts["targetBlank"]) && $atts["targetBlank"] ? "_blank" : "_self",
+			"rel" => !empty($atts["targetBlank"]) && $atts["targetBlank"] ? "noreferrer noopener" : ""
+		]
+	]);
 }
 
 add_shortcode('button', 'shortcode_button');
