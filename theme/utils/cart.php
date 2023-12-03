b3af2d57bdd6a9d5246421b774255f3b55e42588
Dominik Bláha <dominikblaha@Dominikuv-MacBook-Pro.local>
2023-12-03 23:32:51 +0100

    Remove unused scripts

--- theme/utils/cart.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return; // remove if you want to use Cart
-
-require __DIR__ .'/shop/cart.php';
-
-$App->cart = new MangoPress\Shop\Cart($App->getService('session'));
--- theme/utils/get_active_lang_code.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function get_active_lang_code() {
-	global $sitepress;
-	if (isset($sitepress)) { // WPML is enabled
-		return $sitepress->get_current_language();
-	}
-	return explode('_', get_locale())[0];
-}
