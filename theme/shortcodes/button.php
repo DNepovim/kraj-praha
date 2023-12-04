c0dc7e9768dad3d981d26854bb6fa7fddd2b50d0
Dominik Bláha <dominikblaha@Dominikuv-MacBook-Pro.local>
2023-12-04 01:43:34 +0100

    Update button shortcode

--- theme/shortcodes/button.php
+++ theme/shortcodes/button.php
@@ -1,12 +1,15 @@
 <?php
 function shortcode_button($atts) {
+	$targetBlank = in_array("targetBlank", $atts);
+	$fullWidth = in_array("fullWidth", $atts);
 	return renderLatteToString(__DIR__ . '/../views/components/button.latte', [
 		"label" => !empty($atts["label"]) ? $atts["label"] : "",
 		"style" => !empty($atts["style"]) ? $atts["style"] : "inv",
 		"attrs" => [
 			"href" => !empty($atts["link"]) ? $atts["link"] : "",
-			"target" => !empty($atts["targetBlank"]) && $atts["targetBlank"] ? "_blank" : "_self",
-			"rel" => !empty($atts["targetBlank"]) && $atts["targetBlank"] ? "noreferrer noopener" : ""
+			"fullWidth" => $fullWidth,
+			"target" => $targetBlank ? "_blank" : "_self",
+			"rel" => $targetBlank ? "noreferrer noopener" : "",
 		]
 	]);
 }
